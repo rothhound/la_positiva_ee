5c81c9fbe7b7fe1dd3b0ba961c051f71b48fd012
Gianfranco Montoya <gms122@gmail.com>
2013-12-24 02:01:18 -0500

    fundation_plugin-filtros

---
+++ system/expressionengine/third_party/filtros/pi.filtros.php
@@ -0,0 +1,92 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Memberlist Class
+ *
+ * @package     ExpressionEngine
+ * @category        Plugin
+ * @author      Gianfranco Montoya 
+ * @copyright       Copyright (c) 2014, Gianfranco Montoya
+ * @link        http://www.ayuinc.com/
+ */
+
+$plugin_info = array(
+    'pi_name'         => 'Filtros',
+    'pi_version'      => '1.0',
+    'pi_author'       => 'Gianfranco Montoya',
+    'pi_author_url'   => 'http://www.ayuinc.com/',
+    'pi_description'  => 'Allows states qualify Friends',
+    'pi_usage'        => Filtros::usage()
+);
+            
+class Filtros 
+{
+
+    var $return_data = "";
+    // --------------------------------------------------------------------
+
+        /**
+         * Memberlist
+         *
+         * This function returns a list of members
+         *
+         * @access  public
+         * @return  string
+         */
+    public function __construct(){
+        $this->EE =& get_instance();
+        $var = $this->EE->input->post('count');
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Usage
+     *
+     * This function describes how the plugin is used.
+     *
+     * @access  public
+     * @return  string
+     */
+    public static function usage()
+    {
+        ob_start();  ?>
+        The Memberlist Plugin simply outputs a
+        list of 15 members of your site.
+
+            {exp:filtros}
+
+        This is an incredibly simple Plugin.
+            <?php
+        $buffer = ob_get_contents();
+        ob_end_clean();
+        return $buffer;
+    }
+    // END
+
+    public function brand(){
+        ee()->db->distinct('marca');
+        ee()->db->select('marca');
+        $query = ee()->db->get('exp_valor_autos');
+        return $query->num_rows();
+    }
+
+    public function model(){
+        $marca = ee()->TMPL->fetch_param('marca');
+        ee()->db->distinct('modelo');
+        ee()->db->select('modelo');
+        ee()->db->where('marca',$marca);
+        $query = ee()->db->get('exp_valor_autos');
+        return $query->num_rows();
+    }
+
+    public function version(){
+        $modelo = ee()->TMPL->fetch_param('modelo');
+        ee()->db->distinct('version');
+        ee()->db->select('version');
+        ee()->db->where('modelo',$modelo);
+        $query = ee()->db->get('exp_valor_autos');
+        return $query->num_rows();
+    }  
+/* End of file pi.rating.php */
+/* Location: ./system/expressionengine/third_party/rating/pi.rating.php */
