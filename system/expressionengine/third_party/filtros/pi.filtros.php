c5cd572f1ce7a05dbb2ba48897a6e14369c7226e
Gianfranco Montoya <gms122@gmail.com>
2013-12-24 12:58:13 -0500

    filtros-ajax

--- system/expressionengine/third_party/filtros/pi.filtros.php
+++ system/expressionengine/third_party/filtros/pi.filtros.php
@@ -65,28 +65,45 @@ class Filtros
     // END
 
     public function brand(){
+        $form = '<select name="marca" id="marca">' ;
         ee()->db->distinct('marca');
         ee()->db->select('marca');
         $query = ee()->db->get('exp_valor_autos');
-        return $query->num_rows();
+        foreach($query->result() as $row){
+            $form .= '<option value='.$row->marca.'>'.$row->marca.'</option>';
+            $key = $row->marca;
+            $options[$key] = $row->marca;
+        }
+        $form = $form.'</select>';
+        return $form;
     }
 
     public function model(){
+        $form = '<select name="modelo" id="modelo">';
         $marca = ee()->TMPL->fetch_param('marca');
         ee()->db->distinct('modelo');
         ee()->db->select('modelo');
         ee()->db->where('marca',$marca);
         $query = ee()->db->get('exp_valor_autos');
-        return $query->num_rows();
+        foreach($query->result() as $row){
+            $form .= '<option value='.$row->modelo.'>'.$row->modelo.'</option>';
+        }
+        $form = $form.'</select>';
+        return $form;
     }
 
     public function version(){
+        $form = '<select name="version" id="version">';
         $modelo = ee()->TMPL->fetch_param('modelo');
         ee()->db->distinct('version');
         ee()->db->select('version');
         ee()->db->where('modelo',$modelo);
         $query = ee()->db->get('exp_valor_autos');
-        return $query->num_rows();
+        foreach($query->result() as $row){
+            $form .= '<option value='.$row->version.'>'.$row->version.'</option>';
+        }
+        $form = $form.'</select>';
+        return $form;
     } 
 
     public function year(){
